8975d3197a72725ca5365159411820b9c66f4999
Edlinf <627039309@qq.com>
2021-10-25 17:23:32 +0800

    init

---
+++ teachme.py
@@ -0,0 +1,58 @@
+import time
+
+from nonebot.typing import State_T
+from hoshino import Service, priv
+from hoshino.typing import CQEvent
+from collections import defaultdict
+import base64, os, re, asyncio, json, traceback
+from io import BytesIO
+from PIL import Image
+
+sv = Service('teachme', help_='''
+等会再写
+'''.strip())
+
+
+def image_to_base64(img, format='PNG'):
+    output_buffer = BytesIO()
+    img.save(output_buffer, format)
+    byte_data = output_buffer.getvalue()
+    base64_str = base64.b64encode(byte_data)
+    return base64_str
+
+@sv.on_rex(r'^教教\s?id\s?([0-9]+)')
+async def teach_me(bot, ev: CQEvent):
+    match = ev['match']
+    base_img = Image.open(r'C:\Users\Administrator\Desktop\bot\HoshinoBot\hoshino\modules\maimaiDX\static\mai\teach.jpg')
+    path_dir = 'C:/Users/Administrator/Desktop/bot/HoshinoBot/hoshino/modules/maimaiDX/static/mai/cover/'
+    name = match.group(1)
+    pngPath = os.path.join(path_dir, f'{name}.jpg')
+    if not os.path.exists(pngPath):
+        pngPath = os.path.join(path_dir, f'{name}.png')
+    try:
+        tmp_img = Image.open(pngPath)
+        box = (184,327,335,478)
+        region = tmp_img
+        region = region.resize((box[2] - box[0], box[3] - box[1]))
+        base_img.paste(region, (184,327))
+        await bot.send(ev, f'[CQ:image,file=base64://{image_to_base64(base_img).decode()}]')
+    except:
+        await bot.send(ev, '教不了教不了', at_sender=True)
+        
+        
+@sv.on_rex(r'^教教\s?([a-z]+)$')
+async def teach_me(bot, ev: CQEvent):
+    match = ev['match']
+    base_img = Image.open(r'C:\Users\Administrator\Desktop\bot\HoshinoBot\hoshino\modules\maimaiDX\static\mai\teach.jpg')
+    path_dir = 'C:/Users/Administrator/Desktop/bot/HoshinoBot/hoshino/modules/Arcaea/img/song/'
+    name = match.group(1)
+    pngPath = os.path.join(path_dir, f'{name}/base.jpg')
+    try:
+        tmp_img = Image.open(pngPath)
+        box = (184,327,335,478)
+        region = tmp_img
+        region = region.resize((box[2] - box[0], box[3] - box[1]))
+        base_img.paste(region, (184,327))
+        await bot.send(ev, f'[CQ:image,file=base64://{image_to_base64(base_img).decode()}]')
+    except:
+        await bot.send(ev, '教不了教不了', at_sender=True)
