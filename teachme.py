0790a90d25bf118e1f19483b02f0b0c3032cc3f9
Edlinf <627039309@qq.com>
2022-04-08 13:10:09 +0800

    fix bug for png

--- teachme.py
+++ teachme.py
@@ -33,16 +33,25 @@ async def teach_me(bot, ev: CQEvent):
     match = ev['match']
     base_img = Image.open(teach_path)
     name = match.group(1)
-    pngPath = os.path.join(mai_res, f'{name}.jpg')
+    jpgPath = os.path.join(mai_res, f'{name}.jpg')
+    pngPath = os.path.join(mai_res, f'{name}.png')
     try:
-        tmp_img = Image.open(pngPath)
+        tmp_img = Image.open(jpgPath)
         box = (184,327,335,478)
         region = tmp_img
         region = region.resize((box[2] - box[0], box[3] - box[1]))
         base_img.paste(region, (184,327))
         await bot.send(ev, f'[CQ:image,file=base64://{image_to_base64(base_img).decode()}]')
     except:
-        await bot.send(ev, '教不了教不了', at_sender=True)
+        try:
+            tmp_img = Image.open(pngPath)
+            box = (184,327,335,478)
+            region = tmp_img
+            region = region.resize((box[2] - box[0], box[3] - box[1]))
+            base_img.paste(region, (184,327))
+            await bot.send(ev, f'[CQ:image,file=base64://{image_to_base64(base_img).decode()}]')
+        except:
+            await bot.send(ev, '教不了教不了', at_sender=True)
         
         
 @sv.on_rex(r'^教教\s?([a-z]+)$')
